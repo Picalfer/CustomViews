8dbac4ff8ddef6dacdced4a33530aa7f3ced6cf0
Arthur Kozhemyakin <trable228@icloud.com>
2023-05-25 10:23:18 +0300

    test include tag

--- app/src/main/java/com/landfathich/customviews/MainActivity.kt
+++ app/src/main/java/com/landfathich/customviews/MainActivity.kt
@@ -2,10 +2,25 @@ package com.landfathich.customviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.landfathich.customviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        with(binding.bottomButtons) {
+            negativeButton.text = getString(R.string.cancel)
+            positiveButton.text = getString(R.string.ok)
+
+            positiveButton.setOnClickListener {
+                progress.visibility = View.VISIBLE
+                negativeButton.visibility = View.INVISIBLE
+                positiveButton.visibility = View.INVISIBLE
+            }
+        }
     }
 }
