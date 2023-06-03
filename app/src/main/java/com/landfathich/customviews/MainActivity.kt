540d78acb4aa6a0d3ec6e9e51c29eeda094cc54f
Arthur Kozhemyakin <trable228@icloud.com>
2023-06-03 18:32:54 +0300

    handler added

--- app/src/main/java/com/landfathich/customviews/MainActivity.kt
+++ app/src/main/java/com/landfathich/customviews/MainActivity.kt
@@ -1,27 +1,46 @@
 package com.landfathich.customviews
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.landfathich.customviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val token = Any()
+    private val handler = Handler(Looper.getMainLooper())
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
         binding.bottomButtons.setListener {
             if (it == BottomButtonAction.POSITIVE) {
-                binding.bottomButtons.setPositiveButtonText("Updated Ok")
                 binding.bottomButtons.isProgressMode = true
-                Toast.makeText(this, "Positive button pressed", Toast.LENGTH_SHORT).show()
+
+                handler.postDelayed( {
+                    binding.bottomButtons.isProgressMode = false
+                    binding.bottomButtons.setPositiveButtonText("Updated Ok")
+                    Toast.makeText(this, "Positive button pressed", Toast.LENGTH_SHORT).show()
+                }, token, 2000)
+
+
             } else if (it == BottomButtonAction.NEGATIVE) {
                 binding.bottomButtons.setNegativeButtonText("Updated Cancel")
                 Toast.makeText(this, "Negative button pressed", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(token)
     }
 }
